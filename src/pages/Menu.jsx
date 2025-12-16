fdbebd3d2d132a539fe9e0ffc3893c38e3e8ff72
hasanMohameth <hasanmohameth@gmail.com>
2025-12-17 00:25:23 +0330

    Update Menu.jsx

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -9,12 +9,14 @@ const Menu = () => {
 
   useEffect(() => {
     fetchMeals().then(data => setMeals(data));
+   
   }, []);
 
   return (<>
-           <h1 className="text-3xl font-bold p-8   dark:bg-gray-900 text-gray-900 dark:text-gray-100">Menu</h1>
+           <h1 className="text-3xl font-bold p-8 z-10  dark:bg-gray-900 shadow-md p-4 transition-colors duration-500 dark:text-gray-100   
+           ">Menu</h1>
 
-    <div className="p-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6  dark:bg-gray-900 text-gray-900 dark:text-gray-100">
+    <div className="p-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6   dark:bg-gray-900 shadow-md p-4 transition-colors duration-500 dark:text-gray-100">
 
       {meals.map(meal=> <MealCard key={meal.id} meal={meal}  />)}
     </div>
